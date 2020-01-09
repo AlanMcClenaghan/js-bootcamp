1b41bd5db5744c8e1303740e9db294044e8873a6
Alan McClenaghan <AlanM@Alans-Air.home>
2020-01-09 23:33:25 +0000

    Initial Commit - Basic Section

---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+let name = 'Alan McClenaghan';
+
+console.log(name);
+
+let firstName = 'Alan';
+let lastName = 'McClenaghan';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let city = 'London';
+let country = 'UK';
+let location = city + ', ' + country;
+
+console.log(location);
---
+++ basics/temp-conversion.js
@@ -0,0 +1,13 @@
+let fahrenheit = 32;
+
+// Calculate celsius from fahrenheit
+//(32°F − 32) × 5/9 = 0°C
+let celsius = (fahrenheit - 32) * 5/9;
+
+// Calculate kelvin from fahrenheit
+//(32°F − 32) × 5/9 + 273.15 = 273.15K
+let kelvin = (fahrenheit - 32) * 5/9 + 273.15;
+
+console.log(fahrenheit);
+console.log(celsius);
+console.log(kelvin);
