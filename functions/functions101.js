b3dda646a06bf08edf1910882d578db59facb63c
Alan McClenaghan <AlanM@Alans-Air.home>
2020-01-10 00:15:15 +0000

    Functions 101

---
+++ functions/functions101.js
@@ -0,0 +1,35 @@
+// Function - input (argument), code, output (return value)
+
+let greetUser = function () {
+    console.log('Welcome user!')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let square = function (num) {
+    let result = num * num
+    return result
+}
+
+let value = square(3)
+let otherValue = square(10)
+
+console.log(value)
+console.log(otherValue)
+
+// Challenge Area
+
+let convertFahenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius
+}
+
+console.log(convertFahenheitToCelsius(32))
+console.log(convertFahenheitToCelsius(68))
+
+let tempOne = convertFahenheitToCelsius(-36)
+let tempTwo = convertFahenheitToCelsius(102)
+console.log(tempOne)
+console.log(tempTwo)
